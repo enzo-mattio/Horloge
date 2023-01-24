bb5b7bfc41436a34044696d2b1eaa8b568109eb2
EnzoMattio <113895326+EnzoMattio@users.noreply.github.com>
2023-01-24 09:45:44 +0100

    Ajout du réveil et tentative d'ajout d'une pause.

--- Horloge.py
+++ Horloge.py
@@ -1,8 +1,12 @@
 import time
+import keyboard
+
+temps = True
+pause = False
+
 
 def votre_heure(heure, minutes, secondes):
   
-  temps = True
   alarme_secondes = ""
   alarme_heure = ""
   alarme_minutes = ""
@@ -27,9 +31,13 @@ def votre_heure(heure, minutes, secondes):
   print("Quelle type d'affichage voulez-vous ? (EU/UK)")
   
   affichage = input()
-
+  
   # Boucle qui calcule le temps et affiche l'heure selon le type d'affichage demandé
   while temps == True:
+    # Test d'ajout d'une pause
+    # key = keyboard.read_key() 
+    # if key == 'space':
+      # input("Appuyez sur <Entrer> pour continuer")   
 
     secondes += 1
     if secondes > 59 :
@@ -53,7 +61,21 @@ def votre_heure(heure, minutes, secondes):
     if alarme_heure == heure and alarme_minutes == minutes and alarme_secondes == secondes:
       print("C'est l'heure !!!!")
       break
+    
     time.sleep(1)
     
+  # Test d'ajout d'une pause
+  # 
+
+# def pause_key():
+  
+  # key = keyboard.read_key() 
+  # if key == 'space':
+    # input("Appuyez sur <Entrer> pour continuer")
+    
+
+
+  
+
 # Notez l'heure (Heure, Minutes, Secondes)
 votre_heure(23, 59, 50)
